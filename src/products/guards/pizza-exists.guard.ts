658ce9c56b2a0aee67d3d4982608ae8d17bfed47
Christine Wilks <crissxross@gmail.com>
2017-12-18 12:23:20 +0000

    State preload and protection via Guards

---
+++ src/products/guards/pizza-exists.guard.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { tap, map, filter, take, switchMap } from 'rxjs/operators';
+
+import * as fromStore from '../store';
+
+import { Pizza } from '../models/pizza.model';
+
+@Injectable()
+export class PizzaExistsGuard implements CanActivate {
+
+  constructor(private store: Store<fromStore.ProductsState>) { }
+
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+    return this.checkStore().pipe(
+      switchMap(() => {
+        const id = parseInt(route.params.pizzaId, 10);
+        return this.hasPizza(id);
+      })
+    )
+  }
+
+  hasPizza(id: number): Observable<boolean> {
+    return this.store.select(fromStore.getPizzaEntities).pipe(
+      map((entities: { [key: number]: Pizza }) => !!entities[id]), // !! converts to boolean
+      take(1)
+    );
+  }
+
+  checkStore(): Observable<boolean> {
+    return this.store.select(fromStore.getPizzasLoaded).pipe(
+      tap(loaded => {
+        if (!loaded) {
+          this.store.dispatch(new fromStore.LoadPizzas());
+        }
+      }),
+      filter(loaded => loaded),
+      take(1) // completes this observable stream & switchMap above takes over
+    )
+  }
+}
